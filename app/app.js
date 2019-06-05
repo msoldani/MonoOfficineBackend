05e51827a36d7a8aaedc55c6df92e54c03f8314c
msoldani <soldanimirco@gmail.com>
2019-06-05 17:57:33 +0000

    Fine

--- app/app.js
+++ app/app.js
@@ -15,17 +15,17 @@ app.get('/', function (req, res) {
     res.render("login");
 });
 
-app.get('/login/:login/:pass', function (req, res) {
-      MongoClient.connect('mongodb+srv://admin:wenandrea123@andreawen-jytg8.mongodb.net/test?retryWrites=true,{useNewUrlParser: true}', function(err, db) {
+app.get('/login/:username/:password', function (req, res) {
+      MongoClient.connect('mongodb+srv://root:xxx123@soldanimirco-tpwn0.mongodb.net/test?retryWrites=true&w=majority', function(err, db) {
       if (err) {
         throw err;
       }
-      var dbo = db.db("Tecnologie");
-      dbo.collection("Users").findOne({username: req.params.login, password: req.params.pass}, function(err, result) {
+      var dbo = db.db("MonoOfficine");
+      dbo.collection("Utenti").findOne({username: req.params.username, password: req.params.password}, function(err, result) {
         if (err) {
           throw err;
         }
-        if(result){
+       if(result){
             var x = result.username;
             var y = result.password;
             console.log("Accesso eseguito da: " + x);
@@ -34,23 +34,20 @@ app.get('/login/:login/:pass', function (req, res) {
             res.send({result:result})
 
         }else{
-            res.send({result:"Errore"})
+            res.send("Errore");
         }
         db.close();
       })
     });
 });
 
-
-
-
-
 app.post('/registrazione', function (req, res) {
-            MongoClient.connect('mongodb+srv://admin:wenandrea123@andreawen-jytg8.mongodb.net/test?retryWrites=true,{useNewUrlParser: true}', function(err, db) {
+            MongoClient.connect('mongodb+srv://root:xxx123@soldanimirco-tpwn0.mongodb.net/test?retryWrites=true&w=majority', function(err, db) {
               if (err) {throw err;}
-              var dbo = db.db("Tecnologie");
-              var newRist = {username: req.body.login, password: req.body.pass };
-              dbo.collection("Users").insertOne(newRist, function(err, result) {
+              var dbo = db.db("MonoOfficine");
+              var newRist = {username: req.body.username, password: req.body.password};
+              console.log(req.body.username + req.body.password);
+              dbo.collection("Utenti").insertOne(newRist, function(err, result) {
                 if (err) throw err;
                 if (result.result.n == 1){
                     res.send({result:result})
@@ -59,32 +56,16 @@ app.post('/registrazione', function (req, res) {
                     console.log("username: " + x);
                     console.log("password: " + y);
                 }
-                else{res.send({result:"Errore"})}
+                else{res.send("Errore")}
                 db.close();
               });
     });
 });
-
-
-
-app.post('/noleggio', function (req, res) {
-            MongoClient.connect('mongodb+srv://admin:wenandrea123@andreawen-jytg8.mongodb.net/test?retryWrites=true', function(err, db) {
-              if (err) {throw err;}
-              var dbo = db.db("Tecnologie");
-              var newRist = {username:req.body.User, dataInizio: req.body.dataIn, oraInizio: req.body.oraIn , dataFine:req.body.dataFi, oraFine:req.body.oraFi};
-              dbo.collection("Noleggio").insertOne(newRist, function(err, result) {
-                if (err) throw err;
-                db.close();
-              });
-    });
-});
-
-
 
 app.get('/coordinate', function (req, res) {
-      MongoClient.connect('mongodb+srv://admin:wenandrea123@andreawen-jytg8.mongodb.net/test?retryWrites=true,{useNewUrlParser: true}', function(err, db) {
+      MongoClient.connect('mongodb+srv://root:xxx123@soldanimirco-tpwn0.mongodb.net/test?retryWrites=true&w=majority', function(err, db) {
       if (err) {throw err;}
-      var dbo = db.db("Tecnologie");
+      var dbo = db.db("MonoOfficine");
       dbo.collection("Monopattini").find().toArray(function(err, result) {
         if (err) {throw err;}
         if(result){
@@ -96,6 +77,32 @@ app.get('/coordinate', function (req, res) {
     });
 });
 
+
+app.post('/noleggio', function (req, res) {
+    MongoClient.connect('mongodb+srv://root:xxx123@soldanimirco-tpwn0.mongodb.net/test?retryWrites=true&w=majority', function(err, db) {
+        if (err) {throw err;}
+            var dbo = db.db("MonoOfficine");
+            var newRist = {username:req.body.User, dataInizio: req.body.dataIn, oraInizio: req.body.oraIn , dataFine:req.body.dataFi, oraFine:req.body.oraFi};
+            console.log(newRist);
+            dbo.collection("Noleggio").insertOne(newRist, function(err, result) {
+                if (err) throw err;
+                db.close();
+            });
+    });
+});
+
+app.post('/guasto', function (req, res) {
+    MongoClient.connect('mongodb+srv://root:xxx123@soldanimirco-tpwn0.mongodb.net/test?retryWrites=true&w=majority', function(err, db) {
+        if (err) {throw err;}
+            var dbo = db.db("MonoOfficine");
+            var newRist = {idMonopattino:req.body.idMonopattino, problema: req.body.problema, idUser: req.body.idUser , DataSegn:req.body.DataSegn, OraSegn:req.body.OraSegn};
+            console.log(newRist);
+            dbo.collection("Guasti").insertOne(newRist, function(err, result) {
+                if (err) throw err;
+                db.close();
+            });
+    });
+});
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
